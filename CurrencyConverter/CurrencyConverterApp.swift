5b04c37a02b78fad9cd241d79fa7486f0ef6f792
sarathkumarsankar <27sarathkumar.s@gmail.com>
2023-12-25 20:36:37 +0000

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by sa20199164 on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
